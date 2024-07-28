2cdc50e086475298e6bed3dfc433824a6d196201
tommymeWindows <tommyme@windows.com>
2024-07-28 23:49:57 +0800

    🚨 fix deprecated package

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import _ from 'lodash';
