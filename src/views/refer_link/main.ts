e7239b14940ec1804abbee0c5ba7ce57bd6a9ab1
tommyme <chris.b.you@qq.com>
2024-08-16 14:51:45 +0800

    ✨ [refer link] new command to create snippet with selected code for current language

--- src/views/refer_link/main.ts
+++ src/views/refer_link/main.ts
@@ -1,4 +1,7 @@
 import * as vscode from 'vscode';
+import * as os from 'os';
+import * as path from 'path';
+import * as fs from 'fs';
 export class ReferLinkLauncher {
   static async init(context: vscode.ExtensionContext) {
     let disposable;
@@ -95,5 +98,56 @@ export class ReferLinkLauncher {
         vscode.window.showInformationMessage("please select two part to swap.");
       }
     });
+    context.subscriptions.push(disposable);
+    disposable = vscode.commands.registerCommand("bmx.referLink.createSnippet", () => {
+      const editor = vscode.window.activeTextEditor;
+      const homedir = os.homedir();
+      let macos_folder = 'Library/Application Support/Code/User/snippets'
+      let win_folder = 'AppData\\Roaming\\Code\\User\\snippets'
+      if (editor && editor.selections.length === 1) {
+        const langId = editor.document.languageId;
+        let filename = `${langId}.json`;
+        let full_path: string;
+        if (os.platform() === "win32") {
+          full_path = path.join(homedir, win_folder, filename);
+        } else if (os.platform() == "darwin") {
+          full_path = path.join(homedir, macos_folder, filename);
+        } else {
+          return;
+        }
+        fs.access(full_path, fs.constants.F_OK, async (err) => {
+          if (err) {
+            vscode.window.showErrorMessage('file not exists, please create it manually');
+          } else {
+            let jsonData;
+            let _data = fs.readFileSync(full_path, {encoding: 'utf8'})
+            jsonData = JSON.parse(_data);
+            const [firstSelection] = editor.selections;
+            const firstText = editor.document.getText(firstSelection);
+            let newSnip = {
+              prefix: '',
+              body: [],
+              description: '',
+            };
+            const snipname = await vscode.window.showInputBox({
+              placeHolder: 'please input snippet name',
+              prompt: 'snippet name'
+            });
+            if (!snipname || snipname.length === 0) {
+              return;
+            }
+            newSnip.prefix = snipname;
+            newSnip.body = firstText.split("\n");
+            newSnip.description = `template for ${snipname}`;
+            jsonData[snipname] = newSnip;
+            let content_to_write = JSON.stringify(jsonData, null, 4);
+            fs.writeFileSync(full_path, content_to_write);
+          }
+        });
+      } else {
+        vscode.window.showErrorMessage("please select some text to create snippet");
+      }
+    });
+    context.subscriptions.push(disposable);
   }
 }
