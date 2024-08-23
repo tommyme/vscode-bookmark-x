be2e5f777b3951eecbf4cb37b63667ff7a9687c1
tommymeWindows <tommyme@windows.com>
2024-08-24 00:15:54 +0800

    update eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,7 @@ export default tseslint.config(
     rules: {
       "prefer-const": "off",
       "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
       "no-constant-condition": "warn"
     }
   }
